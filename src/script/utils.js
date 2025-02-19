3df47746bd50b041533822de46ac2f60f50389dd
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-02-19 12:40:17 +0700

    remove unused resource

--- src/script/utils.js
+++ src/script/utils.js
@@ -1,18 +1,5 @@
 class Utils {
-    static emptyElement(element) {
-        element.innerHTML = '';
-    }
-
-    static showElement(element) {
-        element.style.display = 'block';
-        element.hidden = false;
-    }
-
-    static hideElement(element) {
-        element.style.display = 'none';
-        element.hidden = true;
-    }
-
+    
     static isValidInteger(newValue) {
         return Number.isNaN(newValue) || Number.isFinite(newValue);
     }
